371d41de4f905ca7fc666f9c9a9503c5e437a349
jessehartloff <hartloff@buffalo.edu>
2017-02-07 02:14:03 -0500

    initial commit

---
+++ src/VariablesLab.java
@@ -0,0 +1,22 @@
+public class VariablesLab {
+
+	public static void main(String[] args){
+		
+		String person = "";
+		String noun1 = "";
+		String noun2 = "";
+		String adjective1 = "";
+		String adjective2 = "";
+		String adverb = "";
+		
+		
+		
+		
+		
+		
+		System.out.print(person + " left " + adverb + " in his " + adjective1 + " car " );
+		System.out.print("after taking a " + noun1 + " from the " +noun2+".\nI guess ");
+		System.out.println(person + " is " + adjective2 + ".");
+	}
+	
+}
